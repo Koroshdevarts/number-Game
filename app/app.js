e8ac03bb93d889556a9a35b74b07a9c5701d1b44
korosh <koroshtaghizadeh75@gmail.com>
2025-08-31 22:29:27 -0700

    newNumberGame

--- app/app.js
+++ app/app.js
@@ -2,19 +2,23 @@ let form = document.getElementById("form")
 let submitBtn = document.getElementById("submitBtn")
 let restartBtn = document.getElementById("restartBtn")
 let spanNumber = document.getElementById("spanNumber")
+let input = document.getElementById("input")
 
 
 
+let health = 7
 spanNumber.textContent = "??"
 let systemNumber = Math.floor(Math.random() * 100)
 form.addEventListener("submit", (e) => {
     e.preventDefault()
     const userGues = Number(e.target.inputForm.value)
     if (systemNumber > userGues) {
-        alert("عددت کوچکتره بیشتر تلاش کن")
+        health--
+        alert(" عددت کوچکتره بیشتر تلاش کن جون های باقیمانده :" + health)
     }
     else if (systemNumber < userGues) {
-        alert("عددت بزرگتره بیشتر تلاش کن")
+        alert(" عددت بزرگتره بیشتر تلاش کن جون های باقیمانده :" + health)
+        health--
     }
     else if (systemNumber === userGues) {
         alert("افرین تو بردی")
@@ -24,9 +28,25 @@ form.addEventListener("submit", (e) => {
         restartBtn.classList.remove("button1")
         submitBtn.classList.add("button1")
         submitBtn.classList.remove("input2")
+        input.classList.add("button1")
+        input.classList.remove("input1")
     }
+    healthLog(health);
     e.target.inputForm.value = "";
 })
 restartBtn.addEventListener("click", () => {
     location.reload();
 })
+function healthLog(health) {
+    if (health === 0) {
+        alert("ببخشید جون هات تموم شدن . دوباره شروع کن")
+        restartBtn.classList.add("display")
+        restartBtn.classList.remove("button1")
+        submitBtn.classList.add("button1")
+        submitBtn.classList.remove("input2")
+        input.classList.add("button1")
+        input.classList.remove("input1")
+        spanNumber.textContent = systemNumber;
+    }
+}
+healthLog(health);
